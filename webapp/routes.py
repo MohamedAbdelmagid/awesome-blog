0f11989140408a655aca53532bee1deed581139c
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-03-03 14:32:08 -0800

    Templates

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,7 +1,28 @@
+from flask import render_template
 from webapp import app
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, Flask!"
+@app.route('/home')
+def home():
+    user = {'username': 'Mohamed Abdelmagid'}
+    posts = [
+        {
+            'author': {'username': 'Mohamed Abdelmagid'},
+            'title': 'Blog Post 1',
+            'content': 'First post content',
+            'date_posted': 'April 20, 2018'
+        },
+        {
+            'author': {'username': 'Ali Mohamed Abdelgadir'},
+            'title': 'Blog Post 2',
+            'content': 'Second post content',
+            'date_posted': 'April 21, 2018'
+        }
+]
+    return render_template('home.html', user=user, posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
