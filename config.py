7470572b45582e4f900378d4a257820b07ced6ee
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-03-22 18:21:34 -0700

    Deployment to Heroku Cloud

--- config.py
+++ config.py
@@ -28,6 +28,8 @@ class Config(object):
 
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
 
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
 
 class ProductionConfig(Config):
     DEBUG = False
